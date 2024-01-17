32dd4ae9ca037b79d79b74d7e9062f68c5754a3a
Ayush K M <ayushkm0000@gmail.com>
2024-01-17 19:23:22 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pydocit",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "astunparse==1.6.3",
+    ],
+    author="Ayush K M",
+    author_email="ayushkm0000@gmail.com",
+    description=" ",
+    url="https://github.com/ayuzzh/pydocit",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
