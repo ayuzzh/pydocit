f96753ed685b2da206da1457c28ff8790a6b762f
Ayush K M <ayushkm0000@gmail.com>
2024-01-17 19:24:32 +0530

    Added pydocit src folder

---
+++ pydocit/builder.py
@@ -0,0 +1,86 @@
+from typing import List, Optional
+import json
+import os
+import ast
+
+from .docstring_extractor import Visitor
+
+
+class Builder:
+    """This class builds the json file which contains
+    all the docstring extracted from the package selected."""
+
+    def __init__(
+            self,
+            name: str,
+            path: str,
+            exclude_directories: Optional[List[str]] = None,
+            recursive_search: bool = True,
+    ):
+        self.name = name
+        self.path: str = path
+        self.exclude_directories = None
+        if exclude_directories is not None:
+            self.exclude_directories: Optional[List[str]] = exclude_directories
+        self.recursive_search: bool = recursive_search
+
+        self.included_packages = []
+        self.included_directories = []
+
+        self.files: List[dict] = []
+
+        self.final_build = {}
+
+    def find_directories(self, path: str):
+        list_of_directories = [d for d in os.listdir(path)]
+
+        is_this_a_package = False
+        if "__init__.py" in list_of_directories and os.path.isfile(os.path.join(path, "__init__.py")):
+            is_this_a_package = True
+            self.included_packages.append(path)
+        else:
+            self.included_directories.append(path)
+
+        for i in list_of_directories:
+            if i.endswith(".py"):
+                self.files.append({
+                    "name": i,
+                    "is_in_package": is_this_a_package,
+                    "path": os.path.join(path, i)
+                })
+            elif os.path.isdir(os.path.join(path, i)):
+                if not i.startswith("."):
+                    if i not in self.exclude_directories:
+                        self.find_directories(os.path.join(path, i))
+
+    def compile_docs(self):
+        self.final_build["name"] = self.name
+
+        self.final_build["packages"] = self.included_packages
+        self.final_build["directories"] = self.included_directories
+
+        self.final_build["content"] = {}
+
+        for i in self.files:
+
+            visitor = Visitor()
+            with open(i["path"], encoding="utf8") as file:
+                print(i["path"])
+                tree = ast.parse(file.read())
+                visitor.call_visit(tree)
+
+            self.final_build["content"][i["name"]] = {
+                "name": i["name"],
+                "is_in_package": i["is_in_package"],
+                "path": i["path"],
+                "docs": visitor.out(),
+            }
+
+
+
+
+
+    def build(self) -> str:
+        self.find_directories(self.path)
+        self.compile_docs()
+        return json.dumps(self.final_build, indent=10)
---
+++ pydocit/docstring_extractor.py
@@ -0,0 +1,98 @@
+import ast
+import astunparse
+
+
+class Visitor(ast.NodeVisitor):
+    """The parsed source code of the file should be passed to extract
+    docstrings and functions and class definitions."""
+
+    def __init__(self):
+        self.classes = {}
+        self.functions = {}
+        self.docstring = {}
+        self.imports = []
+
+        self.current_class = None
+        self.current_function = None
+
+    def visit_ClassDef(self, node):
+
+        bases = []
+        for i in node.bases:
+            bases.append(astunparse.unparse(i))
+
+        self.classes[node.name] = {
+            "name": node.name,
+            "methods": {},
+            "bases": bases,
+            "docstring": ast.get_docstring(node),
+        }
+        self.current_class = node.name
+        self.generic_visit(node)
+        self.current_class = None
+
+    def visit_FunctionDef(self, node):
+        args = {}
+        list_of_args = []
+        for i in node.args.args:
+            _source = astunparse.unparse(i)
+
+            annotation = None
+            if i.annotation is not None:
+                annotation = astunparse.unparse(i.annotation)
+
+            type_comment = None
+            if i.type_comment is not None:
+                type_comment = astunparse.unparse(i.type_comment)
+
+            list_of_args.append(i.arg)
+            args[i.arg] = {
+                "name": i.arg,
+                "annotations": annotation,
+                "default": None,
+                "type_comment": type_comment,
+                "def_source": _source,
+            }
+
+        for m, n in zip(reversed(node.args.defaults), reversed(list_of_args)):
+            default = None
+            if m is not None:
+                default = astunparse.unparse(m)
+            args[n]["default"] = default
+
+        if self.current_function is None:
+            if self.current_class:
+                self.classes[self.current_class]["methods"][node.name] = {
+                    "args": args,
+                    "docstring": ast.get_docstring(node),
+                }
+            else:
+                self.functions[node.name] = {
+                    "args": args,
+                    "docstring": ast.get_docstring(node),
+                }
+
+        # For wrapping up the function
+        self.current_function = node.name
+        self.generic_visit(node)
+        self.current_function = None
+
+    def visit_Import(self, node):
+        for i in node.names:
+            self.imports.append(i.name)
+
+    def visit_ImportFrom(self, node):
+        for i in node.names:
+            self.imports.append(f"{node.module}.{i.name}")
+
+    def call_visit(self, tree):
+        self.docstring = ast.get_docstring(tree)
+        super().visit(tree)
+
+    def out(self):
+        return {
+            "functions": self.functions,
+            "classes": self.classes,
+            "docstring": self.docstring,
+            "imports": self.imports,
+        }
