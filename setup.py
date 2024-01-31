78be3e1bcebc05be83a8dc46e9c7d7cd5101e56f
Ayush K M <ayushkm0000@gmail.com>
2024-01-31 14:17:02 +0530

    ⚙️ Add minimum Python requirement

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+import sys
+if sys.version_info < (3, 9):
+    sys.exit("Only Python3.9 or above is supported")
+
+
 setup(
     name="pydocit",
     version="0.1",
     packages=find_packages(),
     install_requires=[],
+    python_requires=">3.9",
     author="Ayush K M",
     author_email="ayushkm0000@gmail.com",
     description=" ",
